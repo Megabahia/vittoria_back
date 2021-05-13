b42aaca611d8e5bf2e7fcaf37c6be153271a562c
Alexander Ortiz <alexortizi1996@gmail.com>
2021-05-13 16:13:52 -0500

    agregar envio email por bd

--- appVittoria/apps/config/util.py
+++ appVittoria/apps/config/util.py
@@ -1,13 +1,28 @@
 #lib email
 from django.core.mail import EmailMultiAlternatives
+from apps.ADM.vittoria_catalogo.models import Catalogo
 from rest_framework.response import Response
 from rest_framework import status
+from django.core.mail import get_connection
 def sendEmail(subject, txt_content, from_email,to,html_content):
     try:
-        msg = EmailMultiAlternatives(subject, txt_content, from_email, [to])
+        #Realizo la consulta a la bd antes de enviar el email, es lo que ocasiona tener parametrizado el servidor....
+        emailHost=Catalogo.objects.filter(tipo="SMTP_EMAIL",nombre="HOST",state=1).first()
+        emailUsuario=Catalogo.objects.filter(tipo="SMTP_EMAIL",nombre="USUARIO",state=1).first()
+        emailpassword=Catalogo.objects.filter(tipo="SMTP_EMAIL",nombre="PASSWORD",state=1).first()
+        emailPuerto=Catalogo.objects.filter(tipo="SMTP_EMAIL",nombre="PUERTO",state=1).first()
+
+        connection = get_connection(host=emailHost.valor, 
+                                port=emailPuerto.valor, 
+                                username=emailUsuario.valor, 
+                                password=emailpassword.valor, 
+                                use_tls=True) 
+        msg = EmailMultiAlternatives(subject, txt_content, from_email, [to],connection=connection)
         msg.attach_alternative(html_content, "text/html")
         msg.send()
         return True
     except Exception as e: 
         err={"error":'Un error ha ocurrido: {}'.format(e)}  
+        print(err)
         return Response(err, status=status.HTTP_400_BAD_REQUEST)
+
