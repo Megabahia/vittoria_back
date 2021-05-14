f0e7e036855e06c36f683e69b1683caba3f8b364
Alexander Ortiz <alexortizi1996@gmail.com>
2021-05-14 16:41:44 -0500

    cambios

--- appVittoria/apps/ADM/vittoria_catalogo/urls.py
+++ appVittoria/apps/ADM/vittoria_catalogo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from apps.ADM.vittoria_catalogo.views import(
 	catalogo_list,catalogo_create,catalogo_findOne,catalogo_update,catalogo_delete,
-	estado_list, pais_list,tipo_list,catalogo_list_hijo,catalogo_list_hijoNombre
+	estado_list, pais_list,tipo_list,catalogo_list_hijo,catalogo_list_hijoNombre,catalogo_list_hijos
 
 )
 
@@ -21,9 +21,11 @@ urlpatterns = [
 	#PAIS
 	path('list/pais/', pais_list, name="pais_list"), 
 	#TIPO PARAMETRIZACION/CATALOGO
-	path('list/tipo/', tipo_list, name="pais_list"), 
+	path('list/tipo/', tipo_list, name="tipo_list"), 
 	#INFORMACION DEL PADRE
-	path('list/tipo/hijo/<int:pk>', catalogo_list_hijo, name="pais_list"), 
+	path('list/tipo/hijo/<int:pk>', catalogo_list_hijo, name="tipohijo_list"), 
 	#HIJOS DEL TIPO
-	path('list/tipo/hijo/nombre/', catalogo_list_hijoNombre, name="pais_list"), 
+	path('list/tipo/hijo/nombre/', catalogo_list_hijoNombre, name="tijohijonombre_list"), 
+	#buscar todos los nombres que pertenecen al tipo enviado
+	path('list/tipo/hijo/', catalogo_list_hijos, name="tipoPadre__list"), 
 ]
