5287d72296fb7b28bfaee9f080113a387d4c583a
james46007 <rootkaliente@gmail.com>
2021-07-09 16:16:19 -0500

    Nombre Categoria en subcategorias

--- appVittoria/apps/MDP/mdp_subCategorias/serializers.py
+++ appVittoria/apps/MDP/mdp_subCategorias/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 
 from apps.MDP.mdp_subCategorias.models import SubCategorias
-
+from apps.MDP.mdp_categorias.serializers import CategoriasListarSerializer
 
 class SubCategoriasSerializer(serializers.ModelSerializer):
+    categoria = CategoriasListarSerializer(many=False, read_only=True)
     class Meta:
         model = SubCategorias
        	fields = '__all__'
+    def to_representation(self, instance):
+        data = super(SubCategoriasSerializer, self).to_representation(instance)
+        categoria = data.pop('categoria')
+        if categoria['nombre']:
+            data['categoria'] = categoria['nombre']
+        return data
 
 # LISTAR SUB CATEGORIAS
 class SubCategoriasListarSerializer(serializers.ModelSerializer):
